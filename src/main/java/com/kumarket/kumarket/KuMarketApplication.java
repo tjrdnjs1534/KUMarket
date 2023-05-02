76e8ce2c1acd698aa791d34efa766d0d98e4d1a8
SeokWon <tjrdnjs1534@naver.com>
2023-05-02 22:16:52 +0900

    project initialize

---
+++ src/main/java/com/kumarket/kumarket/KuMarketApplication.java
@@ -0,0 +1,13 @@
+package com.kumarket.kumarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KuMarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KuMarketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kumarket/kumarket/KuMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kumarket.kumarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KuMarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
